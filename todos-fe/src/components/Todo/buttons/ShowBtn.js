797a7ffd4d39d83753f7d990ece4e0feee1d8bb7
tunahua <leejunkilowinnive@gmail.com>
2021-11-01 22:29:08 +0800

    1101 practice in front-end

--- todos-fe/src/components/Todo/buttons/ShowBtn.js
+++ todos-fe/src/components/Todo/buttons/ShowBtn.js
@@ -1,11 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const ShowBtn = (props) => {
   return (
     <>
-      <FontAwesomeIcon icon={faEye} className="mr-2" />
-      Show
+      {/* 加上Link 綁定 todoList單一點擊的項目ID */}
+      <Link to={`todo/${props.itemId}`} className="card-footer-item">
+        <FontAwesomeIcon icon={faEye} className="mr-2" />
+        Show
+      </Link>
     </>
   );
 };
